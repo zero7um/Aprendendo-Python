5c58e01374f193143dc8c8a7fd932910b5b02ee2
wesleyoliveiras <oliveirawesleycontato@gmail.com>
2021-02-24 20:41:01 -0300

    Primeiros passos no Python
    Aqui eu inicio meus primeiros momentos desenvolvendo em Python. Nice!

---
+++ aprendendo-python.py
@@ -0,0 +1,38 @@
+variavel = 2
+print('variavel')
+type(variavel)
+
+#Variáveis com valor real 
+
+preco_do_produto = 2.5
+print(preco_do_produto)
+type (preco_do_produto)
+
+#Variável String
+
+variavel = 'texto'
+print(variavel)
+
+#Variável Boolean
+
+verdadeiro = True
+falso = False
+print (verdadeiro)
+print(falso)
+
+#Listas
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+print (lista)
+
+lista2 = [True, False, True, True]
+print(lista2)
+
+lista_kenzie = [1, 'luan', 2, 'Wesley', True, 'Sarah']
+print(lista_kenzie)
+
+
+variavel_1 = 3
+variavel_2 = 4
+if variavel_1 < variavel_2:
+    print('O valor na variavel 1 é menor')
