b6551746564d0f31fc3f8f4c456340508291ce12
wesleyoliveiras <oliveirawesleycontato@gmail.com>
2021-02-26 18:54:45 -0300

    Numeros opostos
    Execicios de numeros opostos ainda não concluído

---
+++ exercico-messi.py
@@ -0,0 +1,17 @@
+#Armazenando valores
+
+la_liga_goals = 43 
+champions_league_goals = 10
+copa_del_rey_goals = 5
+total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals 
+
+print ("O total de gols é")
+print (total_goals)
+
+# Números opostos
+
+number = 1
+
+def opposite(number):
+    return -1 * number
+    print(number)
